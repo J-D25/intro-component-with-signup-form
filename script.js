4fa5d51cbd4b4741d1f468125d396254f47da1a1
jaspyro <martin.baudry@sfr.fr>
2022-05-19 10:49:08 +0200

    message error if empty

--- script.js
+++ script.js
@@ -1,12 +1,57 @@
 const firstName = document.querySelector('#form_firstname');
-const error_firstName = document.querySelector('.span_firstname');
-
 const lastName = document.querySelector('#form_lastname');
-const error_LastName = document.querySelector('.span_lastname');
-
 const email = document.querySelector('#form_email');
-const error_email = document.querySelector('.span_email');
-
 const password = document.querySelector('#form_password');
-const error_password = document.querySelector('.span_password');
+const button = document.querySelector('#button');
 
+button.addEventListener("click", required)
+
+function required(event) {
+    event.preventDefault()
+
+        var firstName_value = document.forms["formulaire"]["form_firstname"].value;
+        var lastName_value = document.forms["formulaire"]["form_lastname"].value;
+        var email_value = document.forms["formulaire"]["form_email"].value;
+        var password_value = document.forms["formulaire"]["form_password"].value;
+
+        if (firstName_value = "") {
+            const span_firstName = document.createElement("SPAN");
+            span.setAttribute("class", "firstName_span");
+            span.setAttribute("class", "span")
+            const text_firstName_span = document.createTextNode("First Name cannot be empty");
+            span_firstName.appendChild(text_firstName_span);
+        
+        } else {
+
+        }
+
+        if (lastName_value = "") {
+            const span_lastName = document.createElement("SPAN");
+            span.setAttribute("class", "lastName_span");
+            span.setAttribute("class", "span");
+            const text_lastName_span = document.createTextNode("Last Name cannot be empty");
+            span_lastName.appendChild(text_lastName_span);
+        } else {
+
+        }
+
+        if (email_value = "") {
+            const span_email = document.createElement("SPAN");
+            span.setAttribute("class", "lastName_span");
+            span.setAttribute("class", "span");
+            const text_email_span = document.createTextNode("email cannot be empty");
+            span_email.appendChild(text_email_span);
+        } else {
+            
+        }
+
+        if (password_value = "") {
+            const span_password = document.createElement("SPAN");
+            span.setAttribute("class", "lastName_span");
+            span.setAttribute("class", "span");
+            const text_password_span = document.createTextNode("Password cannot be empty");
+            span_password.appendChild(text_password_span);
+        } else {
+            
+        }
+}
