5112cc9268b09cfa1e92dfad7a3f7c14c4bc4a4b
jaspyro <martin.baudry@sfr.fr>
2022-05-19 10:07:01 +0200

    add const for form

---
+++ script.js
@@ -0,0 +1,12 @@
+const firstName = document.querySelector('#form_firstname');
+const error_firstName = document.querySelector('.span_firstname');
+
+const lastName = document.querySelector('#form_lastname');
+const error_LastName = document.querySelector('.span_lastname');
+
+const email = document.querySelector('#form_email');
+const error_email = document.querySelector('.span_email');
+
+const password = document.querySelector('#form_password');
+const error_password = document.querySelector('.span_password');
+
