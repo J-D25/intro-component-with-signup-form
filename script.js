d8ada7c68e657e3b4675dd0eb312adee7e4ecf46
jaspyro <martin.baudry@sfr.fr>
2022-05-19 14:00:34 +0200

    .

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ const email = document.querySelector('#form_email');
 const password = document.querySelector('#form_password');
 const button = document.querySelector('#form_validate');
 
+    const span_firstName = document.createElement("SPAN");
+    const text_firstName_span = document.createTextNode("First Name cannot be empty");
+    span_firstName.appendChild(text_firstName_span);
+
+
+
 button.addEventListener("click", required)
 
 function required(event) {
@@ -16,14 +22,11 @@ function required(event) {
         var password_value = document.forms["formulaire"]["form_password"].value;
 
         if (firstName_value == "") {
-            const span_firstName = document.createElement("SPAN");
-            const text_firstName_span = document.createTextNode("First Name cannot be empty");
-            span_firstName.appendChild(text_firstName_span);
-            form.insertBefore(span_firstName, firstName.nextSibling );
-
-            console.log(span_firstName);
+            
+            form.replaceChild(span_firstName, firstName.nextSibling );
         
         } else {
+            span_firstName.remove(); 
             console.log("ok");
         }
 
@@ -31,7 +34,7 @@ function required(event) {
             const span_lastName = document.createElement("SPAN");
             const text_lastName_span = document.createTextNode("Last Name cannot be empty");
             span_lastName.appendChild(text_lastName_span);
-            console.log("Last Name cannot be empty");
+            form.replaceChild(span_lastName, lastName.nextSibling );
         } else {
             console.log("ok");
         }
@@ -40,7 +43,7 @@ function required(event) {
             const span_email = document.createElement("SPAN");
             const text_email_span = document.createTextNode("email cannot be empty");
             span_email.appendChild(text_email_span);
-            console.log("email cannot be empty");
+            form.replaceChild(span_email, email.nextSibling );
         } else {
             console.log("ok"); 
         }
@@ -49,7 +52,7 @@ function required(event) {
             const span_password = document.createElement("SPAN");
             const text_password_span = document.createTextNode("Password cannot be empty");
             span_password.appendChild(text_password_span);
-            console.log("Password cannot be empty");
+            form.replaceChild(span_password, password.nextSibling );
         } else {
             console.log("ok");
         }
