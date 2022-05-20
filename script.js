d8a2c90e65886db618662aeb2d5178cb8c009f63
J-D25 <jd.auzanneau@icloud.com>
2022-05-20 09:22:52 +0200

    script.js : #form_firstname error working

--- script.js
+++ script.js
@@ -1,59 +1,27 @@
-const form = document.querySelector('#form')
-const firstName = document.querySelector('#form_firstname');
-const lastName = document.querySelector('#form_lastname');
-const email = document.querySelector('#form_email');
-const password = document.querySelector('#form_password');
+const form = document.querySelector('form')
 const button = document.querySelector('#form_validate');
-
-    const span_firstName = document.createElement("SPAN");
-    const text_firstName_span = document.createTextNode("First Name cannot be empty");
-    span_firstName.appendChild(text_firstName_span);
-
-
-
 button.addEventListener("click", required)
 
-function required(event) {
-    event.preventDefault()
+const firstName = document.querySelector('#form_firstname');
+const firstName_span = document.createElement("SPAN");
+const firstName_span_text = document.createTextNode("First Name cannot be empty");
+firstName_span.appendChild(firstName_span_text);
 
-        var firstName_value = document.forms["formulaire"]["form_firstname"].value;
-        var lastName_value = document.forms["formulaire"]["form_lastname"].value;
-        var email_value = document.forms["formulaire"]["form_email"].value;
-        var password_value = document.forms["formulaire"]["form_password"].value;
+function required() {
 
-        if (firstName_value == "") {
-            
-            form.replaceChild(span_firstName, firstName.nextSibling );
-        
-        } else {
-            span_firstName.remove(); 
-            console.log("ok");
+    let input = document.getElementById("form_firstname");
+    if (input.validity.valueMissing) {
+        input.setAttribute("class", "form_input_invalid");
+
+        form.replaceChild(firstName_span, firstName.nextSibling);
+        firstName_span.setAttribute("id", "form_firstname_invalid");
+        firstName_span.setAttribute("class", "form_span_invalid");
+    } else {
+        input.removeAttribute("class", "form_input_invalid");
+        firstName_spanInvalid = document.getElementById('form_firstname_invalid');
+        if (firstName_spanInvalid) {
+            firstName_spanInvalid.remove();
         }
-
-        if (lastName_value == "") {
-            const span_lastName = document.createElement("SPAN");
-            const text_lastName_span = document.createTextNode("Last Name cannot be empty");
-            span_lastName.appendChild(text_lastName_span);
-            form.replaceChild(span_lastName, lastName.nextSibling );
-        } else {
-            console.log("ok");
-        }
-
-        if (email_value == "") {
-            const span_email = document.createElement("SPAN");
-            const text_email_span = document.createTextNode("email cannot be empty");
-            span_email.appendChild(text_email_span);
-            form.replaceChild(span_email, email.nextSibling );
-        } else {
-            console.log("ok"); 
-        }
-
-        if (password_value == "") {
-            const span_password = document.createElement("SPAN");
-            const text_password_span = document.createTextNode("Password cannot be empty");
-            span_password.appendChild(text_password_span);
-            form.replaceChild(span_password, password.nextSibling );
-        } else {
-            console.log("ok");
-        }
+    }
 }
+// console.log(document.getElementById("form_firstname").validity)
