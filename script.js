1d9a643575d2bb87f4d14823fd0f7287f98ea3b3
J-D25 <jd.auzanneau@icloud.com>
2022-05-20 11:03:25 +0200

    script.js : invalid input all

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 const form = document.querySelector('form')
 const button = document.querySelector('#form_validate');
-button.addEventListener("click", required)
+const inputs = document.querySelectorAll("input");
+button.addEventListener("click", required);
 
-const firstName = document.querySelector('#form_firstname');
-const firstName_span = document.createElement("SPAN");
-const firstName_span_text = document.createTextNode("First Name cannot be empty");
-firstName_span.appendChild(firstName_span_text);
+function required(){
+    inputs.forEach((input) => {
+    const input_span = document.createElement("SPAN");
+    const input_placeholder = input.placeholder;
+    const cannot = " cannot be empty";
+    const input_span_text = document.createTextNode(input_placeholder + cannot)
+    input_span.appendChild(input_span_text);
 
-function required() {
-
-    let input = document.getElementById("form_firstname");
     if (input.validity.valueMissing) {
         input.setAttribute("class", "form_input_invalid");
 
-        form.replaceChild(firstName_span, firstName.nextSibling);
-        firstName_span.setAttribute("id", "form_firstname_invalid");
-        firstName_span.setAttribute("class", "form_span_invalid");
+        form.replaceChild(input_span, input.nextSibling);
+        input_span.setAttribute("id", input.id+"_invalid");
+        input_span.setAttribute("class", "form_span_invalid");
     } else {
         input.removeAttribute("class", "form_input_invalid");
-        firstName_spanInvalid = document.getElementById('form_firstname_invalid');
-        if (firstName_spanInvalid) {
-            firstName_spanInvalid.remove();
+        input_spanInvalid = document.getElementById(input.id+"_invalid");
+        if (input_spanInvalid) {
+            input_spanInvalid.remove();
         }
     }
+  })
 }
-// console.log(document.getElementById("form_firstname").validity)
